3f1716a93a3c91c856226fd11761ef80bd3e100b
Adam Gardner <adam.gardner@magicmemories.com>
2019-04-19 10:58:00 -1000

    Fix URI for lookup caches

--- lib/puppet/provider/graylog_lookup_cache/graylog_api.rb
+++ lib/puppet/provider/graylog_lookup_cache/graylog_api.rb
@@ -5,7 +5,7 @@ Puppet::Type.type(:graylog_lookup_cache).provide(:graylog_api, parent: Puppet::P
   mk_resource_methods
 
   def self.instances
-    results = get('system/lookup/cache')
+    results = get('system/lookup/caches')
     results['caches'].map do |data|
       cache = new(
         ensure: :present,
@@ -20,7 +20,7 @@ Puppet::Type.type(:graylog_lookup_cache).provide(:graylog_api, parent: Puppet::P
   end
 
   def flush
-    simple_flush("system/lookup/cache",{
+    simple_flush("system/lookup/caches",{
       name: resource[:name],
       title: resource[:display_name],
       description: resource[:description],
