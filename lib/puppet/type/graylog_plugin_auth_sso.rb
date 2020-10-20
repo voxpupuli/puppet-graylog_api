7e3bd93d6463ed7cb912d636940c6c61fa3b6ea4
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2020-10-20 09:31:44 +0300

    Add example for graylog_plugin_auth_sso type

--- lib/puppet/type/graylog_plugin_auth_sso.rb
+++ lib/puppet/type/graylog_plugin_auth_sso.rb
@@ -10,6 +10,15 @@ Puppet::Type.newtype(:graylog_plugin_auth_sso) do
 
     @example
       graylog_auth_sso_plugin_config { 'org.graylog.plugins.foo/config':
+        trusted_proxies: '127.0.0.1/32',
+        username_header: 'REMOTE_USER'
+        require_trusted_proxies: true
+        auto_create_user: true
+        fullname_header: 'displayName'
+        email_header: 'mail'
+        default_email_domain: 'foo.bar'
+        sync_roles: true
+        roles_header: 'fooGroup'
       }
   END_OF_DOC
 
@@ -46,22 +55,22 @@ Puppet::Type.newtype(:graylog_plugin_auth_sso) do
 
   newproperty(:default_role) do
     desc 'The default Graylog role determines whether a user created can access the entire system, or has limited access.'
-    defaultto('Reader')    
+    defaultto('Reader')
   end
 
   newproperty(:require_trusted_proxies, boolean: true, parent: Puppet::Property::Boolean) do
     desc 'Enable this to require the request containing the SSO header as directly coming from a trusted proxy. This is highly recommended to avoid header injection.
-    The current subnet setting is: 127.0.0.1/32, 0:0:0:0:0:0:0:1/128. You can configure the setting in the Graylog server configuration file.'    
-    defaultto(true)        
+    The current subnet setting is: 127.0.0.1/32, 0:0:0:0:0:0:0:1/128. You can configure the setting in the Graylog server configuration file.'
+    defaultto(true)
   end
 
   newproperty(:auto_create_user, boolean: true, parent: Puppet::Property::Boolean) do
-    desc 'Enable this if Graylog should automatically create a user account for externally authenticated users. If disabled, an administrator needs to manually create a user account.'        
+    desc 'Enable this if Graylog should automatically create a user account for externally authenticated users. If disabled, an administrator needs to manually create a user account.'
     defaultto(false)
   end
 
   newproperty(:sync_roles, boolean: true, parent: Puppet::Property::Boolean) do
-    desc 'Enable this if Graylog should automatically synchronize the roles of the user, with that specified in the HTTP header. Only existing roles in Graylog will be added to the user.'    
+    desc 'Enable this if Graylog should automatically synchronize the roles of the user, with that specified in the HTTP header. Only existing roles in Graylog will be added to the user.'
     defaultto(false)
   end
 
@@ -69,6 +78,6 @@ Puppet::Type.newtype(:graylog_plugin_auth_sso) do
     desc 'Prefix of the HTTP header, can contain a comma-separated list of roles in one header, otherwise all headers with that prefix will be recognized.'
     defaultto('')
   end
-  
+
   autorequire('graylog_api') {'api'}
-end  
+end
